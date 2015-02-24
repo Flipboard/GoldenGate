07fa39441b9f0fc6af4c3b79e7a427b8571dc791
Emil Sjölander <sjolander.emil@gmail.com>
2015-02-24 11:32:12 -0800

    Re-use gson instance

--- api/src/main/java/com/flipboard/goldengate/GsonJsonSerializer.java
+++ api/src/main/java/com/flipboard/goldengate/GsonJsonSerializer.java
@@ -3,13 +3,15 @@ package com.flipboard.goldengate;
 import com.google.gson.Gson;
 
 public class GsonJsonSerializer implements JsonSerializer {
+    
+    private Gson gson = new Gson();
 
     public <T> String toJson(T stuff) {
-        return new Gson().toJson(stuff);
+        return gson.toJson(stuff);
     }
 
     public <T> T fromJson(String json, Class<T> type) {
-        return new Gson().fromJson(json, type);
+        return gson.fromJson(json, type);
     }
 
 }
